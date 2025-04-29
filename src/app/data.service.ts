4eba1abc85f48c8ef7694f6af7e6754b82a4f64d
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 <zander.templin@gmail.com>
2025-04-29 11:43:13 -0500

    Initial connection to firestore is working! Able to start working on storing data and retrieving it.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@angular/core';
-import { MuscleGroup, Exercise, Set as DataSet, ExerciseSet } from '../data types/data-types';
+import {
+  MuscleGroup,
+  Exercise,
+  Set as DataSet,
+  ExerciseSet,
+} from '../data types/data-types';
 import { populateDataMapRando, createTestDataMap } from '../utils/generateData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
   dataMap: Map<Date, ExerciseSet[]> = new Map();
-  
-  constructor() 
-  { 
+
+  constructor() {
     this.dataMap = populateDataMapRando();
   }
 
-  getTotalVolumeForDay(date: Date)
-  {
-    if (!this.dataMap.has(date))
-      return null;
+  getTotalVolumeForDay(date: Date) {
+    if (!this.dataMap.has(date)) return null;
     let exerciseSets = this.dataMap.get(date);
-    if (!exerciseSets)
-      return null;
+    if (!exerciseSets) return null;
     let total = 0;
-    for (const exerciseSet of exerciseSets)
-      total += exerciseSet.totalVolume;
+    for (const exerciseSet of exerciseSets) total += exerciseSet.totalVolume;
     return total;
   }
-
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { User } from '../../data types/data-types';
+import { FetchServiceService } from '../fetch-service.service';
 
 @Component({
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  users?: User[];
+  fetchService = inject(FetchServiceService);
 
+  ngOnInit() {
+    this.fetchService.getUsers().subscribe((users) => (this.users = users));
+  }
 }
